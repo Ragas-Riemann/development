0a3ba479cb97f0d934b66370d8e352f70a1869db
Ragas-Riemann <riemann.ragas@urios.edu.ph>
2025-05-13 07:38:57 +0000

    Riemann Ragas

---
+++ app/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import HelloWorld
+from .views import ContactListView
+from .views import ContactUpdateDetailView
+from .exam_views import ChatView
+
+#from . import views
+# from . import views
+
+urlpatterns = [
+    path('hello/', HelloWorld.as_view(), name='hello_world'),
+    path('contact/', ContactListView.as_view(), name='contact_new'),
+    path('contact/<int:contact_id>/', ContactListView.as_view(), name='contact_detail'),
+    path('contacts/', ContactListView.as_view(), name='contact_list'),
+    path('contacts/<int:contact_id>/', ContactUpdateDetailView.as_view(), name='contact_update_detail'),
+
+]
